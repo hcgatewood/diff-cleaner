b95ead6cc6b1554f5668774ffc54711361a52f0a
Hunter Gatewood <hcg@mit.edu>
2016-07-15 15:06:51 -0700

    Testing trim spec

--- scripts/install.js
+++ scripts/install.js
@@ -1,14 +1,15 @@
-var message =
-    '\n' +
-    '-> Installing diff-cleaner...\n' +
-    '-> Caveats:\n' +
-    '\t- Must have a git hooks manager. `npm install --save-dev husky` is recommended.\n' +
-    '\t- Add the following to your gulpfile.js:\n' +
-    "\t\t- require('./node_modules/diff-cleaner/precommit.js');\n" +
-    "\t\t- require('./node_modules/diff-cleaner/trim.js');\n" +
-    '\t- Add the following to your package.json scripts:\n' +
-    '\t\t- "precommit": "gulp precommit,"\n' +
-    '\t\t- "trim": "gulp trim"\n' +
-    '\n';
+const message = `
+
+-> Installing diff-cleaner...
+-> Caveats:
+   - Must have a git hooks manager. \`npm install --save-dev husky\` is recommended.
+   - Add the following to your gulpfile.js:
+      - require('./diff-cleaner/precommit.js');
+      - require('./diff-cleaner/trim.js');
+   - Add the following to your package.json scripts:
+      - "precommit": "gulp precommit,"
+      - "trim": "gulp trim"
+
+`;
 
 console.log(message);
